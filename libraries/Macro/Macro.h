b760f9a81547fc99404d28ebc86057079fcf7627
rcardenas01 <artiecardenas@gmail.com>
2022-11-07 10:17:26 -0500

    Initial commit

---
+++ libraries/Macro/Macro.h
@@ -0,0 +1,18 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+#include <Ardruino.h>
+#include <String.h>
+
+class Macro
+{
+private:
+  struct KeyCombo
+  {
+    char press;
+    String message;
+  };
+public:
+};
+
+#endif //MACRO_H
