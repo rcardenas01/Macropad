d4fe9058206a33c6fed4735f73f2f959bef3257f
rcardenas01 <artiecardenas@gmail.com>
2022-11-17 00:24:05 -0500

    More code changes

--- Macropad.ino
+++ Macropad.ino
@@ -1,15 +1,22 @@
 #include <String.h>
 #include <Keyboard.h>
-#define MAX_SIZE 5
+#define MAX_SIZE 7
 
 class Macro
 {
 private:
   struct KeyCombo
   {
-    char press;
-    arduino::String message;
-    KeyCombo(char op, String message_in) : press(op), message(message_in) { }
+    char op;
+    String message;
+    
+    KeyCombo(char press, String message_in) : press(op), message(message_in) { }
+    KeyCombo() : press(0), message("") { }
+
+    void execute() 
+    {
+      if (op == 'p')
+    }
   };
 
   String name;
@@ -17,15 +24,23 @@ private:
   KeyCombo key_strokes[MAX_SIZE];
   
 public:
-  Macro(String name_in) : name(name_in), num_keystrokes(0) {  }
+  Macro(String name_in) : name(name_in), num_keystrokes(0) { }
 
   int add(char op, String message)
   {
     if(num_keystrokes == MAX_SIZE) return 1;
 
-    key_strokes[num_keystrokes++] = {}
+    key_strokes[num_keystrokes++] = KeyCombo(op, message);
+    return 0;
+    
   }
-  void execute();
+  void execute() 
+  {
+    for(int i = 0; i < num_keystrokes; ++i)
+    {
+
+    }
+  }
   void clear_macro();
 };
 
